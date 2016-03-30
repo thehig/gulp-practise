303f42b3b4639c12e9667d00730879826da5ad0f
David Higgins <thehig@gmail.com>
2016-03-30 20:36:48 +0100

    Gulp coffee working

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var gutil = require('gulp-util');
 var jshint = require('gulp-jshint');
+var sourcemaps = require('gulp-sourcemaps');
+var coffee = require('gulp-coffee');
 
 // configure the jshint task
 gulp.task('jshint', function() {
@@ -10,6 +12,11 @@ gulp.task('jshint', function() {
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
+gulp.task('coffee', function(){
+	return gulp.src('tests/**/*.coffee')
+		.pipe(coffee().on('error', gutil.log))
+		.pipe(gulp.dest('./public/'));
+});
 
 gulp.task('default', function(){
 	return gutil.log("Gulp is running");
