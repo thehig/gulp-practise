12df26d21fbdf899ce2f7a2867d24d4638e055cd
David Higgins <thehig@gmail.com>
2016-04-01 15:05:55 +0100

    Create first working casper test against localhost

--- tests/casper-sample.js
+++ tests/casper-sample.js
@@ -1,23 +1,12 @@
-// var casper = require('casper').create();
+casper.test.begin('Localhost is working', 2, function suite(test) {
+    casper.start("http://localhost:3000/", function() {
+        test.assertTitle("CasperJS Test", "the title is casperJS Test");
+        test.assertEvalEquals(function(){
+            return __utils__.findOne(".target").innerText;
+        }, "Hello Ben", "the script executed");
+    });
 
-casper.test.begin('Add 1 + 1 and get 2', 2, function(test) {
-	test.assertEquals(1 + 1, 2);    
-	test.assertEquals(2+2 , 4);    
-	test.done();
+    casper.run(function() {
+        test.done();
+    });
 });
-
-// casper.thenOpen('http://phantomjs.org', function() {
-//     this.echo(this.getTitle());
-// });
-
-// casper.thenOpen('http://localhost:3000', function() {
-//     this.echo(this.getTitle());
-
-//     // this.assertEvalEquals(function() {
-//     //     return __utils__.findOne('.target').textContent;
-//     // }, "Hello Ben");
-
-//     // this.assertEquals(1 + 1, 2);
-// });
-
-// casper.run();
