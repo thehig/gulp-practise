f744eb26af7ba20619e4eb5e49764d36d4796c39
Ben Murphy <ben@vonbismark.com>
2016-04-11 16:33:18 +0100

    Add click through tests for the form
    * Add required class to 2 fields
    * Add some extra id's for section purposes

--- tests/formtests/casper-formtest.js
+++ tests/formtests/casper-formtest.js
@@ -1,11 +1,10 @@
-casper.test.begin('Basic DOM Tests', 5, function suite(test) {
+casper.test.begin('Basic DOM Tests', 10, function suite(test) {
 
 	casper.start("http://localhost:8080/formtest.html", function() {
 		// Test Against the page title
 		var pagetitle = this.getTitle();
 		test.assertEquals(pagetitle, "Hardcoded Form")
 
-		
 
 		// 	// Dropdown Tests
 		// =========================================
@@ -45,8 +44,39 @@ casper.test.begin('Basic DOM Tests', 5, function suite(test) {
 		test.assertEval(function() {
 			return __utils__.findAll(".textbox").length == 2;
         }, "There should be 2 text boxes");
-		// // Check the "required" attribute for the 1st textbox
-		// test.assertEquals(textbox[0].hasAttribte("required"), true);
+		
+
+
+
+		// Count the required Fields
+		test.assertEval(function(){
+			return __utils__.findAll(".required").length == 2;
+		}, "There should be two required fields");
+
+		// "User" actions
+		// =========================================
+		
+		// Make a selection on the first dropdown
+		this.click("#comboBox1");
+		this.click("#comboBox1:nth-child(2)");
+        test.assertSelectorHasText("#comboBox1", "option1")
+
+		// Make a selection on the second dropdown
+		this.click("#comboBox2");
+		this.click("#comboBox2:nth-child(2)");
+		test.assertSelectorHasText("#comboBox2", "option1")
+		// Make a selection on the second radio button
+		this.click("#radio2");
+
+		// Fill out the two textboxes
+		this.sendKeys("#textbox1", 'some value');
+		test.assertSelectorHasText("#textbox1", "some value")
+		this.sendKeys("#textbox2", 'some other value');
+		test.assertSelectorHasText("#textbox2", "some other value")
+		// Click the submit button
+		this.click('.btn-submit');
+
+
 	});
 
 	// End of tests
@@ -54,73 +84,5 @@ casper.test.begin('Basic DOM Tests', 5, function suite(test) {
         test.done();
     });
 });
-// casper.test.begin("Basic DOM Tests", 4, function (test) {
-
-
-// 	// Dropdown Tests
-// 	// =========================================
-// 	// Count the different dropdowns
-// 	var dropdowns = document.querySelectorAll(".combobox");
-// 	test.assertEquals(dropdowns.length, 2);
-
-// 	// Count the different dropdowns options
-// 	var dropdownsOption = document.querySelectorAll(".combobox combo-option");
-// 	test.assertEquals(dropdownsOption.length, 8);
-
-// 	// The first dropdown should have the required attribute
-// 	test.assertQEquals(dropdowns[0].hasAttribte("required"), true);
-// 	// the second dropdown should not have the required attribute
-// 	test.assertQEquals(dropdowns[1].hasAttribte("required"), false);
-
-// 	// Radio Button Tests
-// 	// =========================================
-
-// 	// Count the radio buttons
-// 	var radioButton = document.querySelectorAll(".btn-radio");
-// 	test.assertEquals(radioButton.length, 3);
-// 	// Check the "checked" attribute for the 3rd radio button
-// 	test.assertEquals(radioButton[2].hasAttribte("checked"), true);
-
-// 	// Text Box Tests
-// 	// =========================================
-
-// 	// Count the Text boxes
-// 	var textbox = document.querySelectorAll(".textbox");
-// 	test.assertEquals(textbox.length, 2);
-// 	// Check the "required" attribute for the 1st textbox
-// 	test.assertEquals(textbox[0].hasAttribte("required"), true);
-
-
-// 	// Click through tests
-// 	//  ========================================
-// 	// this.click(.combobox[0])
-
-
-// });
-
-
-// // "User" actions
-// // =========================================
-// casper.then(function(){
-// 	// Make a selection on the first dropdown
-// 	this.click(dropdowns[0]);
-// 	this.click(dropdownsOption[2]);
-
-// 	// Make a selection on the first dropdown
-// 	this.click(dropdowns[1]);
-// 	this.click(dropdownsOption[3]);
-
-// 	// Make a selection on the second radio button
-// 	this.click(radioButton[1]);
-
-// 	// Fill out the two textboxes
-// 	this.sendKeys(textbox[0], 'some value');
-// 	this.sendKeys(textbox[1], 'some other value');
-
-// 	// Click the submit button
-// 	this.click('btn-submit');
-
-
-// });
 
 // casper.run();
